66fc7c1a9be44f7c7ab35291dce5838bfe228a8e
aidp-vane <aidp@sktai.io>
2023-04-04 14:34:04 +0900

    bigquery example

--- dags/dag_aidp.py
+++ dags/dag_aidp.py
@@ -9,10 +9,11 @@ from textwrap import dedent
 import pendulum
 from airflow import DAG
 from airflow.providers.sktvane.operators.nes import NesOperator
+from airflow.providers.sktvane.sensors.gcp import BigQueryPartitionSensor
 
 
 with DAG(
-    "dag_nes",
+    "dag_aidp",
     default_args={"retries": 2},
     description="DAG with own plugins",
     # schedule="*/2 * * * *",
@@ -23,9 +24,23 @@ with DAG(
 
     dag.doc_md = __doc__
 
+    sensor = BigqueryPartitionSensor(
+        task_id="sense_bq_part",
+        project_id="skt-datahub",
+        dataset_id="x1112596",
+        table_id="ob_roaming_country_copy",
+        partition="2023-04-01",
+    )
+    sensor.doc_md = dedent(
+        """\
+    #### BigQuery partition sensor
+    BigQuery 파티션 센서 예제
+    """
+    )
+
     nes_task = NesOperator(
         task_id="NES_Task",
-        input_nb="https://github.com/sktaiflow/notebooks/blob/master/test/skt_version_test.ipynb",
+        input_nb="https://github.com/sktaiflow/notebooks/blob/master/test/sample_notebook.ipynb",
     )
     nes_task.doc_md = dedent(
         """\
@@ -33,3 +48,5 @@ with DAG(
     NES를 활용한 task 예제
     """
     )
+
+    sensor >> nes_task
